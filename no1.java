93d34494b022c6cb1b6ad7861ba06d4103979dfe
aditya_6706154184 <aditradoti@gmail.com>
2017-04-06 09:11:50 +0700

    Update no1.java

--- no1.java
+++ no1.java
@@ -8,8 +8,8 @@ public class Fibonacci {
         fib[0] = 0;
         fib[1] = 1;
          
-        for(int i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
+        for(int i = 0; i < n; i++) {
+            fib[i] = fib[i-2] + fib[i-4];
         }
          
         for (int i = 0; i < n; i++) {
